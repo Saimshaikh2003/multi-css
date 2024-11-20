40eaac065a9afcee4668d7ebeefef370fb527335
SAIM SHAIKH <saimshaikh0003@gmail.com>
2024-11-20 15:34:01 +0500

    multi-webpage

--- src/app/watches/page.tsx
+++ src/app/watches/page.tsx
@@ -1,26 +1,63 @@
-import React from 'react'
-import Image from 'next/image'
-
-
+import React from "react";
+import Image from "next/image";
 
 const Watches = () => {
-
   const watchData = [
-    { id: 1, name: 'Rolex', price: 12000 , description : 'luxury watch' , Image: '/pic1.jpg' },
-    { id: 2, name: 'omega', price: 20000 , description : 'luxury watch' , Image: '/pic2.jpg' },
-    { id: 3, name: 'Adidas', price: 80000 , description : 'luxury watch', Image: '/pic5.jpeg' },
-     { id: 6, name: 'Beetling', price: 90000 , description : 'luxury watch', Image: '/pic9.jpg' },
-    { id: 7, name: 'Rado', price: 32000 , description : 'luxury watch', Image: '/pic10.jpg' },
-    { id: 8, name: 'seiko', price: 175000 , description : 'luxury watch', Image: '/pic11.jpg' },
-    
-  ]
+    {
+      id: 1,
+      name: "Rolex",
+      price: 12000,
+      description: "luxury watch",
+      Image: "/public/pic1.jpg",
+    },
+    {
+      id: 2,
+      name: "omega",
+      price: 20000,
+      description: "luxury watch",
+      Image: "/public/pic2.jpg",
+    },
+    {
+      id: 3,
+      name: "Adidas",
+      price: 80000,
+      description: "luxury watch",
+      Image: "/public/pic5.jpeg",
+    },
+    {
+      id: 6,
+      name: "Beetling",
+      price: 90000,
+      description: "luxury watch",
+      Image: "/public/pic9.jpg",
+    },
+    {
+      id: 7,
+      name: "Rado",
+      price: 32000,
+      description: "luxury watch",
+      Image: "/public/pic10.jpg",
+    },
+    {
+      id: 8,
+      name: "seiko",
+      price: 175000,
+      description: "luxury watch",
+      Image: "/public/pic11.jpg",
+    },
+  ];
 
   return (
     <div>
       <div className="watches">
         {watchData.map((watch) => (
-          <div key = {watch.id} className="watch-card">
-             <Image src={watch.Image} alt={watch.name} width={298} height={263} />
+          <div key={watch.id} className="watch-card">
+            <Image
+              src={watch.Image}
+              alt={watch.name}
+              width={298}
+              height={263}
+            />
             <h3>{watch.name}</h3>
             <p>{watch.description}</p>
             <div className="price">${watch.price}</div>
@@ -29,7 +66,7 @@ const Watches = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Watches
+export default Watches;
