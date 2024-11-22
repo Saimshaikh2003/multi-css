9fbdf47b6795e4b86a36530ee3feafef57f215f2
SAIM SHAIKH <saimshaikh0003@gmail.com>
2024-11-22 17:20:22 +0500

    multi-webpage

--- src/app/watches/page.tsx
+++ src/app/watches/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 const Watches = () => {
   const watchData = [
-    { id: 1, name: "Rolex", price: 12000, description: "luxury watch", image: "/pic1.jpg" },
-    { id: 2, name: "Omega", price: 20000, description: "luxury watch", image: "/pic2.jpg" },
-    { id: 3, name: "Adidas", price: 80000, description: "luxury watch", image: "/pic5.jpeg" },
-    { id: 6, name: "Beetling", price: 90000, description: "luxury watch", image: "/pic9.jpg" },
-    { id: 7, name: "Rado", price: 32000, description: "luxury watch", image: "/pic10.jpg" },
-    { id: 8, name: "Seiko", price: 175000, description: "luxury watch", image: "/pic11.jpg" },
+    { id: 1, name: 'Rolex', price: 12000, description: 'luxury watch', image: '/pic1.jpg' },
+    { id: 2, name: 'Omega', price: 20000, description: 'luxury watch', image: '/pic2.jpg' },
+    { id: 3, name: 'Adidas', price: 80000, description: 'luxury watch', image: '/pic5.jpeg' },
+    { id: 6, name: 'Beetling', price: 90000, description: 'luxury watch', image: '/pic9.jpg' },
+    { id: 7, name: 'Rado', price: 32000, description: 'luxury watch', image: '/pic10.jpg' },
+    { id: 8, name: 'Seiko', price: 175000, description: 'luxury watch', image: '/pic11.jpg' },
   ];
 
   return (
@@ -16,12 +16,13 @@ const Watches = () => {
       <div className="watches">
         {watchData.map((watch) => (
           <div key={watch.id} className="watch-card">
-            <Image
-              src={watch.image}
-              alt={watch.name}
-              width={293}
-              height={270}
-              className="watch-image"
+            {/* Replace <img> with <Image /> */}
+            <Image 
+              src={watch.image} 
+              alt={watch.name} 
+              width={293} 
+              height={270} 
+              unoptimized
             />
             <h3>{watch.name}</h3>
             <p>{watch.description}</p>
