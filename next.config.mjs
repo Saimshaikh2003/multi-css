6493127449932600e213dc465d345e7f52075a9e
SAIM SHAIKH <saimshaikh0003@gmail.com>
2024-11-22 17:30:13 +0500

    multi-webpage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+module.exports = {
+    images: {
+      unoptimized: true,
+    },
+  };
+  export default nextConfig;
+  
